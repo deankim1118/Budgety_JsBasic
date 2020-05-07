cec80f331044ee384172a3fad90f12da994196f3
deankim1118 <deankim1118@gmail.com>
2020-05-07 16:44:10 -0400

    #2.Setting up the first event listeners
    #2.Setting up the first event listeners
    'keypress' and 'click'

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// BUDGET CONTROLLER
 let budgetController = (function () {
-  let b = 5;
+  // Some code
 })();
 
+// UI CONTROLLER
 let UIController = (function () {
-  let a = 10;
+  // Some code
 })();
 
+// GLOBAL CONTROLLER
 let controller = (function (butgetCtrl, UICtrl) {
-  let a = 10 + b;
+  let ctrlAddItem = function () {
+    // 1. Get the field input value
+    // 2. Add the item to the butget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  };
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
