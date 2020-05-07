48677fa6463bbacc071afaef913ea437f3e51ad8
deankim1118 <deankim1118@gmail.com>
2020-05-07 17:39:07 -0400

    #3.Reading the input data

--- app.js
+++ app.js
@@ -5,13 +5,34 @@ const budgetController = (function () {
 
 // UI CONTROLLER
 const UIController = (function () {
-  // Some code
+  const DOMstring = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputBtn: '.add__value',
+  };
+  // 1. Get the field input value
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstring.inputType).value,
+        description: document.querySelector(DOMstring.inputDescription).value,
+        value: document.querySelector(DOMstring.inputBtn).value,
+      };
+    },
+    getDOMstring: function () {
+      return DOMstring;
+    },
+  };
 })();
 
 // GLOBAL CONTROLLER
 const controller = (function (butgetCtrl, UICtrl) {
+  const DOM = UICtrl.getDOMstring();
+
   let ctrlAddItem = function () {
     // 1. Get the field input value
+    let input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the butget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
