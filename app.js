958eb691785ff1472bc43f04fb85e7a54f23c24c
deankim1118 <deankim1118@gmail.com>
2020-05-07 22:20:41 -0400

    #6.Add new Item into out Date structure
    Adding a new item into budgetController

--- app.js
+++ app.js
@@ -22,6 +22,28 @@ const budgetController = (function () {
       inc: 0,
     },
   };
+
+  return {
+    addItem: function (type, des, val) {
+      let newItem;
+
+      // Create new ID
+      if (data.allItem[type].length > 0) {
+        ID = data.allItem[type][allItem[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      // Create newItem instance based on input.type of 'exp' and 'inc'
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+      // Push the newItem into the data structure
+      data.allItem[type].push(newItem);
+      console.log(data.allItem);
+    },
+  };
 })();
 
 // UI CONTROLLER
@@ -60,9 +82,12 @@ const controller = (function (butgetCtrl, UICtrl) {
   };
 
   let ctrlAddItem = function () {
+    let input, newItem;
+
     // 1. Get the field input value
-    let input = UICtrl.getInput();
+    input = UICtrl.getInput();
     // 2. Add the item to the butget controller
+    newItem = butgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
